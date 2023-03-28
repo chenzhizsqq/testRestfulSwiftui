165b92f8285b92b9370124ec18abecb6e053c5b7
chenzhizsqq <chenzhi@qq.com>
2023-03-28 12:03:32 +0900

    Initial Commit

---
+++ testRestfulSwiftui/testRestfulSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  testRestfulSwiftuiApp.swift
+//  testRestfulSwiftui
+//
+//  Created by chenzhizs on 2023/03/28.
+//
+
+import SwiftUI
+
+@main
+struct testRestfulSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
