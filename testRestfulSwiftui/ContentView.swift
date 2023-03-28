9b28395629c93db9ac454fd4209fac28571073c3
chenzhizsqq <chenzhi@qq.com>
2023-03-28 12:19:57 +0900

    暂时弄一个网上的接口，去测试本地iphone的环境

--- testRestfulSwiftui/ContentView.swift
+++ testRestfulSwiftui/ContentView.swift
@@ -6,16 +6,51 @@
 //
 
 import SwiftUI
+import Alamofire
+
+struct webSiteApiTestViewPost: Decodable, Identifiable {
+    let id: Int
+    let title: String
+    let body: String
+}
 
 struct ContentView: View {
+       
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        webSiteApiTestView()
+    }
+    
+    //这是自己的一个接口
+    func selfRESTfulApiView() -> some View {
+        
+        ZStack {
         }
-        .padding()
+    }
+    
+    
+    //这是网上的一个接口
+    @State private var posts: [webSiteApiTestViewPost] = []
+    func webSiteApiTestView() -> some View {
+        
+        ZStack {
+            List(posts, id: \.id) { post in
+                VStack(alignment: .leading) {
+                    Text(post.title)
+                        .font(.headline)
+                    Text(post.body)
+                        .font(.subheadline)
+                }
+            }
+            .onAppear {
+                AF.request("https://jsonplaceholder.typicode.com/posts")
+                    .validate()
+                    .responseDecodable(of: [webSiteApiTestViewPost].self) { response in
+                        guard let posts = response.value else { return }
+                        self.posts = posts
+                    }
+            }
+        }
+        
     }
 }
 
